6fa0368333deeeec2e8278fe03524174c33192c7
Sergey Chechaev <2netme@gmail.com>
2017-03-06 00:16:48 +0300

    add credo

--- mix.exs
+++ mix.exs
@@ -45,7 +45,10 @@ defmodule ExPropisju.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.14", only: :dev},
-     {:earmark, "~> 1.1", only: :dev}]
+    [
+      {:ex_doc, "~> 0.14", only: :dev},
+      {:earmark, "~> 1.1", only: :dev},
+      {:credo, "~> 0.6.1", only: [:dev, :test], runtime: false}
+    ]
   end
 end
